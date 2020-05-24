50f1e007b5f8e451b41973c6168b5ea3ab203eb5
alicoskun <alicoskun@outlook.com>
2020-05-25 02:00:08 +0300

    Updated console log

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ app.get("/", (req, res, next) => {
     res.json(Math.random() > .5 ? 'yes' : 'no');
 });
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Server running on port 5000");
+
+let port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
